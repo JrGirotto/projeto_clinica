868f69d51990fcca8732b693a66e5885f2d40d40
jrgirotto <jrgirotto@live.com>
2024-02-05 19:02:21 -0300

    Configuração Camera

--- fe_lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
+++ fe_lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
@@ -1,25 +1,28 @@
 import 'package:asyncstate/asyncstate.dart';
-import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
-import 'package:fe_lab_clinicas_core/src/theme/lab_clinicas_theme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
+import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
+import 'package:fe_lab_clinicas_core/src/theme/lab_clinicas_theme.dart';
+
 class LabClinicasCoreConfig extends StatelessWidget {
   const LabClinicasCoreConfig({
-    super.key,
+    Key? key,
     this.binding,
     this.pages,
     this.pagesBuilders,
     this.modules,
     required this.title,
-  });
+    this.didStart,
+  }) : super(key: key);
 
   final ApplicationBindings? binding;
   final List<FlutterGetItPageRouter>? pages;
   final List<FlutterGetItPageBuilder>? pagesBuilders;
   final List<FlutterGetItModule>? modules;
   final String title;
+  final VoidCallback? didStart;
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +35,9 @@ class LabClinicasCoreConfig extends StatelessWidget {
         return AsyncStateBuilder(
           loader: LabClinicasLoader(),
           builder: (navigatorObserver) {
+            if (didStart != null) {
+              didStart!();
+            }
             return MaterialApp(
               debugShowCheckedModeBanner: false,
               theme: LabClinicasTheme.lightTheme,
