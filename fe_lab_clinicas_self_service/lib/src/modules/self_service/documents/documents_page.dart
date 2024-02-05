9c18ab466aba3f1fb1abb9ba89f838c4ea9e40e6
jrgirotto <jrgirotto@live.com>
2024-02-05 10:14:42 -0300

    Initial

---
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/documents/documents_page.dart
@@ -0,0 +1,30 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/widget/lab_clinicas_self_service_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class DocumentsPage extends StatelessWidget {
+  const DocumentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      appBar: LabClinicasSelfServiceAppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            width: sizeOf.width * .85,
+            margin: const EdgeInsets.only(top: 18),
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(children: [
+              Image.asset('assets/images/folder.png'),
+              const SizedBox(height: 24),
+              Text('ADICIONAR DOCUMENTOS',
+              
---
+++ fe_lab_clinicas_self_service/lib/src/services/user_login_service_impl.dart
@@ -0,0 +1,32 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/repositories/user/user_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import './user_login_service.dart';
+
+class UserLoginServiceImpl implements UserLoginService {
+  UserLoginServiceImpl({required this.userRepository});
+
+  final UserRepository userRepository;
+
+  @override
+  Future<Either<ServiceException, Unit>> execute(
+      String email, String password) async {
+    final loginResult = await userRepository.login(email, password);
+
+    switch (loginResult) {
+      case Left(value: AuthError()):
+        return Left(ServiceException(message: 'Erro ao realizar login'));
+
+      case Left(value: AuthUnauthorizedException()):
+        return Left(ServiceException(message: 'Login ou senha inválidos'));
+
+      case Right(value: final accessToken):
+        final sp = await SharedPreferences.getInstance();
+        sp.setString(LocalStorageConstants.accessToken, accessToken);
+        return Right(unit);
+    }
+  }
+}
+
+//TODO: Só funciona fazendo cast, gera o token, mas dá erro e não vai pra pagina depois do login
