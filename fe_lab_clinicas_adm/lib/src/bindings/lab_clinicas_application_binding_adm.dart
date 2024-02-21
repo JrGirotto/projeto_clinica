ef1522b8dafb2d979016523477aa957a51d8784a
jrgirotto <jrgirotto@live.com>
2024-02-21 13:24:09 -0300

    Configuração arquivos g.dart - D5A8

--- fe_lab_clinicas_adm/lib/src/bindings/lab_clinicas_application_binding_adm.dart
+++ fe_lab_clinicas_adm/lib/src/bindings/lab_clinicas_application_binding_adm.dart
@@ -1,6 +1,7 @@
-import 'package:fe_lab_clinicas_adm/src/core/env.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter_getit/flutter_getit.dart';
+
+import 'package:fe_lab_clinicas_adm/src/core/env.dart';
 
 class LabClinicasApplicationBindingAdm extends ApplicationBindings {
   @override
